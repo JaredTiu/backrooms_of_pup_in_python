bbe40ae990f6b1c285e50f3422c7c7f640c118d7
unknown <jaredtiu1706@gmail.com>
2025-01-23 17:21:12 +0800

    set the parameters for the ends and rows of the map

--- map_making.py
+++ map_making.py
@@ -5,3 +5,5 @@ from numba import njit
 #this algorithm is for making of the maps. 
 
 def main(size):
+    map = np.random.choice(np.array([True, False]), (size, size))
+    map[0,:], map[size-1,:], map[:,0], map[:,size-1] = (True, True, True, True)
